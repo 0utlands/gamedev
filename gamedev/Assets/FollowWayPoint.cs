7588933bb268f9e5ef0393482ed68d013cb78e8d
William Bosley <will.bosley555@gmail.com>
2023-03-16 14:30:37 +0000

    Add guard waypoints system

---
+++ gamedev/Assets/FollowWayPoint.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowWayPoint : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] waypoints;
+    int currentWaypoint = 0;
+    public float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, waypoints[currentWaypoint].transform.position) < 0.5f)
+        {
+            currentWaypoint++;
+        }
+
+        if (currentWaypoint >= waypoints.Length)
+        {
+            currentWaypoint = 0;
+        }
+
+        agent.SetDestination(waypoints[currentWaypoint].transform.position);
+
+    
+    }
+}
