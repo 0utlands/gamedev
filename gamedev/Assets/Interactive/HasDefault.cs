ea6a7853aa5cb4b24141c637418f021877a19ca2
William Bosley <will.bosley555@gmail.com>
2023-04-25 17:13:12 +0100

    Added new interface for objs with default state
    This interface can be applied to objects that need to have a default state, such as doors.

---
+++ gamedev/Assets/Interactive/HasDefault.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface HasDefault
+{
+
+    
+    //get if this map part is in its default state. if false, the guard who sees it should return it to its default state.
+    public bool GetIfInDefaultState()
+    {
+        return true;
+    }
+
+    //should be overriden in doors / things with hasdefault, to return all the gameobjects which can be interacted with to change the state of the thing.
+    public GameObject[] GetInteractors()
+    {
+        return new GameObject[0];
+    }
+}
