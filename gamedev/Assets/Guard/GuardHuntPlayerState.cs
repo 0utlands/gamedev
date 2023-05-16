1e86c6ff1cbc73fbcd8c13b52fb761d980033acb
William Bosley <will.bosley555@gmail.com>
2023-05-16 16:45:11 +0100

    added new uard huinting player state

---
+++ gamedev/Assets/Guard/GuardHuntPlayerState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardHuntPlayerState : GuardBaseState
+{
+    public override void enterState(GuardStateManager guard)
+    {
+        //Debug.Log("SEE PLAYER STATE ENTERED");
+        //guard.agent.speed = 1.0f;
+        guard.agent.isStopped = false;
+    }
+
+    public override void updateState(GuardStateManager guard)
+    {
+        
+        //the gaurd is alert to the player. set the guards Ai destination to be the players location, and make their speed a bit slower so they creep towards them.
+        guard.agent.SetDestination(guard.guardSenses.player.transform.position);
+        //coudl move this to a fixedUpdateState function to make it a bit less cumbersome for computer
+        if (guard.getIfGuardShouldStopHuntingPlayer())
+        {
+            guard.SwitchState(guard.defaultState);
+        }
+        if (guard.getIfGuardShouldChasePlayer())
+        {
+            guard.SwitchState(guard.chasePlayerState);
+        }
+    }
+}
